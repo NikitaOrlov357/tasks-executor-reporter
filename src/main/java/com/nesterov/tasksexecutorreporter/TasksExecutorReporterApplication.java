024561862d68e4e039969ef7ef741682d0ba2d4e
BabyBurrito <mywketep00@mail.ru>
2022-05-22 12:27:07 +0300

    init

---
+++ src/main/java/com/nesterov/tasksexecutorreporter/TasksExecutorReporterApplication.java
@@ -0,0 +1,13 @@
+package com.nesterov.tasksexecutorreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksExecutorReporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksExecutorReporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nesterov/tasksexecutorreporter/TasksExecutorReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesterov.tasksexecutorreporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksExecutorReporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
