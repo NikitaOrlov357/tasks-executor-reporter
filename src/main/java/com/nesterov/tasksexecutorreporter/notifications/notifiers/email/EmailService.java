dffbf27870ad3960828d9ff8c5edf59130e104eb
BabyBurrito <mywketep00@mail.ru>
2022-06-18 11:29:46 +0300

    сделали dummy mode

--- src/main/java/com/nesterov/tasksexecutorreporter/notifications/notifiers/email/EmailService.java
+++ src/main/java/com/nesterov/tasksexecutorreporter/notifications/notifiers/email/EmailService.java
@@ -1,20 +1,23 @@
 package com.nesterov.tasksexecutorreporter.notifications.notifiers.email;
 
-import com.nesterov.tasksexecutorreporter.dao.CommandDao;
+import com.nesterov.tasksexecutorreporter.configs.applicationConfigs.ExternalConfigs;
 import com.nesterov.tasksexecutorreporter.dao.OwnerDao;
 import com.nesterov.tasksexecutorreporter.dto.Owner;
 import com.nesterov.tasksexecutorreporter.notifications.notifiers.Notifier;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service("e-mail")
 @RequiredArgsConstructor
 public class EmailService implements Notifier {
 
     private final JavaMailSender javaMailSender ;
     private final OwnerDao ownerDao;
+    private final ExternalConfigs.NotificationsConfig notificationsConfig;
 
 //    @Override
 //    public void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment) throws MessagingException, FileNotFoundException {
@@ -30,6 +33,14 @@ public class EmailService implements Notifier {
 
     @Override
     public void makeNotify(long ownerId, String message) {
+        if (notificationsConfig.isDummyModeNotificationsEmailEnable()){
+            dummyMode(ownerId, message);
+        } else {
+            realNotify(ownerId, message);
+        }
+    }
+
+    public void realNotify(long ownerId, String message){
         Owner owner = ownerDao.getOwnerById(ownerId);
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(owner.getEMail());
@@ -37,4 +48,9 @@ public class EmailService implements Notifier {
         simpleMailMessage.setText(message);
         javaMailSender.send(simpleMailMessage);
     }
+
+    public void dummyMode(long ownerId, String message){
+        log.info("DUMMY message={} to owner={} was send", message,ownerId);
+    }
+
 }
