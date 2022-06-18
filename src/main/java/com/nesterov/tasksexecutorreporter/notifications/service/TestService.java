5914054b2702cadaf8fd2e62c999e2442c7685ae
BabyBurrito <mywketep00@mail.ru>
2022-06-18 11:29:46 +0300

    сделали dummy mode

--- src/main/java/com/nesterov/tasksexecutorreporter/notifications/service/TestService.java
+++ src/main/java/com/nesterov/tasksexecutorreporter/notifications/service/TestService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService {
     public TestService (NotifierService notifierService){
-
+        notifierService.makeNotify(1,true);
     }
 
 }
